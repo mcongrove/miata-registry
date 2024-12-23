e7e14a76071afd766e17d852c453c18c48029460
Matthew Congrove <mattcongrove@gmail.com>
2024-12-23 05:19:04 -0600

    Build fixes

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,5 +1,5 @@
 declare module '*.svg?react' {
-    import React from 'react';
-    const SVGComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    export default SVGComponent;
+	import * as React from 'react';
+	const SVGComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+	export default SVGComponent;
 }
