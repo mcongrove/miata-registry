bd1d1045c4a537ce12387523ba550f6a40b335b9
Matthew Congrove <mattcongrove@gmail.com>
2025-01-05 06:47:08 -0600

    Update CSP

--- src/components/CSP.tsx
+++ src/components/CSP.tsx
@@ -30,11 +30,11 @@ export const CSP = () => {
 			default-src 'self' ${devCSP}; 
 			script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googleapis.com *.clerk.accounts.dev *.cloudflareinsights.com clerk.miataregistry.com challenges.cloudflare.com *.fontawesome.com ${devCSP}; 
 			worker-src 'self' blob: ${devCSP}; 
-			connect-src 'self' *.googleapis.com *.clerk.accounts.dev clerk.miataregistry.com https://api.miataregistry.com https://store.miataregistry.com https://api.github.com vpic.nhtsa.dot.gov challenges.cloudflare.com *.sentry.io ${devCSP}; 
+			connect-src 'self' *.googleapis.com *.clerk.accounts.dev clerk.miataregistry.com https://api.miataregistry.com https://store.miataregistry.com https://api.github.com vpic.nhtsa.dot.gov challenges.cloudflare.com *.sentry.io *.clerk.miataregistry.com ${devCSP}; 
 			img-src 'self' data: *.googleapis.com *.gstatic.com *.clerk.com https://store.miataregistry.com flagcdn.com ${devCSP}; 
 			style-src 'self' 'unsafe-inline' *.googleapis.com *.gstatic.com ${devCSP}; 
 			font-src 'self' *.gstatic.com *.fontawesome.com ${devCSP};
-			frame-src 'self' *.clerk.accounts.dev challenges.cloudflare.com ${devCSP};
+			frame-src 'self' *.clerk.accounts.dev *.clerk.miataregistry.com challenges.cloudflare.com ${devCSP};
 		`
 			.replace(/\s+/g, ' ')
 			.trim();
