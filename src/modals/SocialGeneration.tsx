d54f65a2209923f143399a02569014fba8eed236
Matthew Congrove <mattcongrove@gmail.com>
2025-01-20 03:15:58 -0600

    Allow color override for social image generation

--- src/modals/SocialGeneration.tsx
+++ src/modals/SocialGeneration.tsx
@@ -18,11 +18,14 @@
 
 import { useEffect, useRef, useState } from 'react';
 import Symbol from '../assets/symbol.svg?react';
+import { Select } from '../components/form/Select';
 import { TextField } from '../components/form/TextField';
 import { Modal } from '../components/Modal';
 import { TCar } from '../types/Car';
 import { colorMapSocial } from '../utils/car';
+import { toTitleCase } from '../utils/common';
 import { formatLocation } from '../utils/location';
+
 type SocialGenerationProps = {
 	isOpen: boolean;
 	onClose: () => void;
@@ -46,11 +49,19 @@ export function SocialGeneration({
 			city: car.current_owner?.city,
 		})}`
 	);
+	const [colorOverride, setColorOverride] = useState<string>(
+		car.edition?.color?.toLowerCase() || ''
+	);
 	const [imageStyles, setImageStyles] = useState<string>(
 		JSON.stringify({
 			objectPosition: 'left bottom',
 		})
 	);
+
+	const colorOptions = Object.keys(colorMapSocial).map((color) => ({
+		value: color,
+		label: toTitleCase(color),
+	}));
 
 	useEffect(() => {
 		setSocialText(generateSocialText());
@@ -139,163 +150,174 @@ export function SocialGeneration({
 		URL.revokeObjectURL(url);
 	};
 
-	const generateSocialImage = () => (
-		<div
-			ref={socialImageRef}
-			onClick={handleImageClick}
-			style={{
-				width: '1080px',
-				height: '1080px',
-				color: colorMapSocial[
-					car.edition?.color?.toLowerCase() || 'various'
-				],
-				transform: 'scale(0.415)',
-				top: '-316px',
-				left: '-316px',
-				position: 'absolute',
-				cursor: 'pointer',
-				backgroundColor: '#FFF',
-			}}
-		>
+	const generateSocialImage = () => {
+		const activeColor =
+			colorOverride || car.edition?.color?.toLowerCase() || '';
+
+		return (
 			<div
+				ref={socialImageRef}
+				onClick={handleImageClick}
 				style={{
+					width: '1080px',
+					height: '1080px',
+					color: colorMapSocial[activeColor],
+					transform: 'scale(0.415)',
+					top: '-316px',
+					left: '-316px',
 					position: 'absolute',
-					top: '309px',
-					right: 0,
-					bottom: 0,
-					left: 0,
-					width: '100%',
-					height: '771px',
-					overflow: 'hidden',
-					zIndex: 0,
+					cursor: 'pointer',
+					backgroundColor: '#FFF',
 				}}
 			>
-				<img
-					src={`https://store.miataregistry.com/car/${car.id}.jpg`}
+				<div
 					style={{
+						position: 'absolute',
+						top: '309px',
+						right: 0,
+						bottom: 0,
+						left: 0,
 						width: '100%',
-						height: '100%',
-						objectFit: 'cover',
-						maxWidth: 'unset',
-						...getParsedStyles(),
+						height: '771px',
+						overflow: 'hidden',
+						zIndex: 0,
+					}}
+				>
+					<img
+						src={`https://store.miataregistry.com/car/${car.id}.jpg`}
+						style={{
+							width: '100%',
+							height: '100%',
+							objectFit: 'cover',
+							maxWidth: 'unset',
+							...getParsedStyles(),
+						}}
+					/>
+				</div>
+
+				<div
+					style={{
+						position: 'absolute',
+						top: 0,
+						left: 0,
+						right: 0,
+						width: '100%',
+						height: '675px',
+						zIndex: 1,
+					}}
+				>
+					<svg
+						width="100%"
+						height="100%"
+						viewBox="0 0 1080 675"
+						preserveAspectRatio="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M0,0 L1080,0 L1080,309 L0,675 Z"
+							fill="#E8EBEA"
+						/>
+					</svg>
+				</div>
+
+				<Symbol
+					style={{
+						width: '400px',
+						height: 'auto',
+						position: 'absolute',
+						top: '50px',
+						left: '50px',
+						zIndex: 2,
 					}}
 				/>
-			</div>
 
-			<div
-				style={{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					right: 0,
-					width: '100%',
-					height: '675px',
-					zIndex: 1,
-				}}
-			>
-				<svg
-					width="100%"
-					height="100%"
-					viewBox="0 0 1080 675"
-					preserveAspectRatio="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path d="M0,0 L1080,0 L1080,309 L0,675 Z" fill="#E8EBEA" />
-				</svg>
-			</div>
-
-			<Symbol
-				style={{
-					width: '400px',
-					height: 'auto',
-					position: 'absolute',
-					top: '50px',
-					left: '50px',
-					zIndex: 2,
-				}}
-			/>
-
-			<div
-				style={{
-					position: 'absolute',
-					top: '71px',
-					right: '50px',
-					border: `5px solid ${colorMapSocial[car.edition?.color?.toLowerCase() || 'various']}`,
-					boxShadow: '0 4px 10px 5px rgb(23 46 40 / 0.15)',
-					borderRadius: '9999px',
-					height: '72px',
-					padding: '5px 30px',
-					fontSize: '35px',
-					zIndex: 2,
-				}}
-			>
-				{car.sequence !== null ? (
-					<>
-						{car.sequence &&
-							car.sequence > (car.edition?.total_produced || 0) &&
-							'Serial '}
-						#<span style={{ fontWeight: 500 }}>{car.sequence}</span>{' '}
-						<span style={{ opacity: 0.6 }}>of</span>{' '}
-						<span style={{ fontWeight: 500 }}>
-							{(
-								car.edition?.total_produced || 0
-							).toLocaleString()}
-						</span>
-					</>
-				) : (
-					<>
-						<span style={{ fontWeight: 500 }}>One</span>{' '}
-						<span style={{ opacity: 0.6 }}>of</span>{' '}
-						<span style={{ fontWeight: 500 }}>
-							{(
-								car.edition?.total_produced || 0
-							).toLocaleString()}
-						</span>
-					</>
-				)}
-			</div>
-
-			<div
-				style={{
-					position: 'absolute',
-					top: '215px',
-					left: '50px',
-					zIndex: 2,
-				}}
-			>
-				<h2
+				<div
 					style={{
-						fontWeight: 500,
-						fontSize: '75px',
-						letterSpacing: '-0.025em',
+						position: 'absolute',
+						top: '71px',
+						right: '50px',
+						border: `5px solid ${colorMapSocial[activeColor]}`,
+						boxShadow: '0 4px 10px 5px rgb(23 46 40 / 0.15)',
+						borderRadius: '9999px',
+						height: '72px',
+						padding: '5px 30px',
+						fontSize: '35px',
+						zIndex: 2,
 					}}
 				>
-					Now on the Registry
-				</h2>
+					{car.sequence !== null ? (
+						<>
+							<span style={{ opacity: 0.6 }}>
+								{car.sequence &&
+									car.sequence >
+										(car.edition?.total_produced || 0) &&
+									'Serial '}
+							</span>
+							<span style={{ fontWeight: 500 }}>
+								#{car.sequence}
+							</span>{' '}
+							<span style={{ opacity: 0.6 }}>of</span>{' '}
+							<span style={{ fontWeight: 500 }}>
+								{(
+									car.edition?.total_produced || 0
+								).toLocaleString()}
+							</span>
+						</>
+					) : (
+						<>
+							<span style={{ fontWeight: 500 }}>One</span>{' '}
+							<span style={{ opacity: 0.6 }}>of</span>{' '}
+							<span style={{ fontWeight: 500 }}>
+								{(
+									car.edition?.total_produced || 0
+								).toLocaleString()}
+							</span>
+						</>
+					)}
+				</div>
 
-				<h4
+				<div
 					style={{
-						fontWeight: 500,
-						fontSize: '40px',
-						opacity: 0.6,
-						width: '730px',
-						lineHeight: 1.25,
-						display: 'flex',
-						flexWrap: 'wrap',
-						rowGap: '0rem',
-						columnGap: '0.75rem',
+						position: 'absolute',
+						top: '215px',
+						left: '50px',
+						zIndex: 2,
 					}}
 				>
-					{imageSubtitle.split(' in ').map((part, index) => (
-						<span key={index} style={{ whiteSpace: 'nowrap' }}>
-							{index === 0 ? '' : ' in '}
-							{part}
-						</span>
-					))}
-				</h4>
+					<h2
+						style={{
+							fontWeight: 500,
+							fontSize: '75px',
+							letterSpacing: '-0.025em',
+						}}
+					>
+						Now on the Registry
+					</h2>
+
+					<h4
+						style={{
+							fontWeight: 500,
+							fontSize: '40px',
+							opacity: 0.6,
+							width: '730px',
+							lineHeight: 1.25,
+							display: 'flex',
+							flexWrap: 'wrap',
+							rowGap: '0rem',
+							columnGap: '0.75rem',
+						}}
+					>
+						{imageSubtitle.split(' in ').map((part, index) => (
+							<span key={index} style={{ whiteSpace: 'nowrap' }}>
+								{index === 0 ? '' : ' in '}
+								{part}
+							</span>
+						))}
+					</h4>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	};
 
 	return (
 		<>
@@ -311,6 +333,18 @@ export function SocialGeneration({
 					</div>
 
 					<div className="flex flex-col gap-2">
+						<Select
+							value={colorOverride}
+							onChange={(
+								e: React.ChangeEvent<HTMLSelectElement>
+							) => setColorOverride(e.target.value)}
+							placeholder="Select Color Override"
+							options={[
+								{ value: '', label: 'Select Color Override' },
+								...colorOptions,
+							]}
+						/>
+
 						<TextField
 							value={imageSubtitle}
 							onChange={(
